25bbb3bdf28d738a1761a01a62de456bf13c0150
AdnanKhan45 <ak187429@gmail.com>
2024-10-01 21:46:30 +0500

    Initial Commit

---
+++ Weather-Swift/Weather_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftApp.swift
+//  Weather-Swift
+//
+//  Created by Muhammad Adnan on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Weather-SwiftTests/Weather_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftTests.swift
+//  Weather-SwiftTests
+//
+//  Created by Muhammad Adnan on 01/10/2024.
+//
+
+import Testing
+@testable import Weather_Swift
+
+struct Weather_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
